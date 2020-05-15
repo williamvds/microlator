e7740d365adea8f3ff8ddf4407d131e1aaf8eedc
williamvds <william@williamvds.me>
2020-05-15 11:39:07 +0100

    Move instruction table definition to source file

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -58,7 +58,8 @@ constexpr void CPU::reset() {
 void CPU::execute() {
 	const auto opcode = read(pc++);
 
-	const auto& instruction = instructions[opcode];
+	static auto instructions = CPU::getInstructions();
+	const auto instruction = instructions[opcode];
 	if (!instruction.function)
 		return;
 
@@ -522,4 +523,83 @@ void CPU::oTXS(ValueStore) {
 void CPU::oTYA(ValueStore) {
 	accumulator = indexY;
 	setZeroNegative(accumulator);
+}
+
+constexpr auto CPU::getInstructions() -> Instructions {
+	using C = CPU;
+	using M = AddressMode;
+
+	return {{
+		{&C::oBRK         }, {&C::oORA, M::IndX}, {                 }, {},
+		{                 }, {&C::oORA, M::Zpg }, {&C::oASL, M::Zpg }, {},
+		{&C::oPHP         }, {&C::oORA, M::Imm }, {&C::oASL, M::A   }, {},
+		{                 }, {&C::oORA, M::Abs }, {&C::oASL, M::Abs }, {},
+		{&C::oBPL, M::Rel }, {&C::oORA, M::IndY}, {                 }, {},
+		{                 }, {&C::oORA, M::ZpgX}, {&C::oASL, M::ZpgX}, {},
+		{&C::oCLC         }, {&C::oORA, M::AbsY}, {                 }, {},
+		{                 }, {&C::oORA, M::AbsX}, {&C::oASL, M::AbsX}, {},
+
+		{&C::oJSR, M::Abs }, {&C::oAND, M::IndX}, {                 }, {},
+		{&C::oBIT, M::Zpg }, {&C::oAND, M::Zpg }, {&C::oROL, M::Zpg }, {},
+		{&C::oPLP         }, {&C::oAND, M::Imm }, {&C::oROL, M::A   }, {},
+		{&C::oBIT, M::Abs }, {&C::oAND, M::Abs }, {&C::oROL, M::Abs }, {},
+		{&C::oBMI, M::Rel }, {&C::oAND, M::IndY}, {                 }, {},
+		{                 }, {&C::oAND, M::ZpgX}, {&C::oROL, M::ZpgX}, {},
+		{&C::oSEC         }, {&C::oAND, M::AbsY}, {                 }, {},
+		{                 }, {&C::oAND, M::AbsX}, {&C::oROL, M::AbsX}, {},
+
+		{&C::oRTI         }, {&C::oEOR, M::IndX}, {                 }, {},
+		{                 }, {&C::oEOR, M::Zpg }, {&C::oLSR, M::Zpg }, {},
+		{&C::oPHA         }, {&C::oEOR, M::Imm }, {&C::oLSR, M::A   }, {},
+		{&C::oJMP, M::Abs }, {&C::oEOR, M::Abs }, {&C::oLSR, M::Abs }, {},
+		{&C::oBVC, M::Rel }, {&C::oEOR, M::IndY}, {                 }, {},
+		{                 }, {&C::oEOR, M::ZpgX}, {&C::oLSR, M::ZpgX}, {},
+		{&C::oCLI         }, {&C::oEOR, M::AbsY}, {                 }, {},
+		{                 }, {&C::oEOR, M::AbsX}, {&C::oLSR, M::AbsX}, {},
+
+		{&C::oRTS         }, {&C::oADC, M::IndX}, {                 }, {},
+		{                 }, {&C::oADC, M::Zpg }, {&C::oROR, M::Zpg }, {},
+		{&C::oPLA         }, {&C::oADC, M::Imm }, {&C::oROR, M::A   }, {},
+		{&C::oJMP, M::Ind }, {&C::oADC, M::Abs }, {&C::oROR, M::Abs }, {},
+		{&C::oBVS, M::Rel }, {&C::oADC, M::IndY}, {                 }, {},
+		{                 }, {&C::oADC, M::ZpgX}, {&C::oROR, M::ZpgX}, {},
+		{&C::oSEI         }, {&C::oADC, M::AbsY}, {                 }, {},
+		{                 }, {&C::oADC, M::AbsX}, {&C::oROR, M::AbsX}, {},
+
+		{                 }, {&C::oSTA, M::IndX}, {                 }, {},
+		{&C::oSTY, M::Zpg }, {&C::oSTA, M::Zpg }, {&C::oSTX, M::Zpg }, {},
+		{&C::oDEY         }, {                 }, {&C::oTXA         }, {},
+		{&C::oSTY, M::Abs }, {&C::oSTA, M::Abs }, {&C::oSTX, M::Abs }, {},
+		{&C::oBCC, M::Rel }, {&C::oSTA, M::IndY}, {                 }, {},
+		{&C::oSTY, M::ZpgX}, {&C::oSTA, M::ZpgX}, {&C::oSTX, M::ZpgY}, {},
+		{&C::oTYA         }, {&C::oSTA, M::AbsY}, {&C::oTXS         }, {},
+		{                 }, {&C::oSTA, M::AbsX}, {                 }, {},
+
+		{&C::oLDY, M::Imm }, {&C::oLDA, M::IndX}, {&C::oLDX, M::Imm }, {},
+		{&C::oLDY, M::Zpg }, {&C::oLDA, M::Zpg }, {&C::oLDX, M::Zpg }, {},
+		{&C::oTAY         }, {&C::oLDA, M::Imm }, {&C::oTAX         }, {},
+		{&C::oLDY, M::Abs }, {&C::oLDA, M::Abs }, {&C::oLDX, M::Abs }, {},
+		{&C::oBCS, M::Rel }, {&C::oLDA, M::IndY}, {                 }, {},
+		{&C::oLDY, M::ZpgX}, {&C::oLDA, M::ZpgX}, {&C::oLDX, M::ZpgY}, {},
+		{&C::oCLV         }, {&C::oLDA, M::AbsY}, {&C::oTSX         }, {},
+		{&C::oLDY, M::AbsX}, {&C::oLDA, M::AbsX}, {&C::oLDX, M::AbsY}, {},
+
+		{&C::oCPY, M::Imm }, {&C::oCMP, M::IndX}, {                 }, {},
+		{&C::oCPY, M::Zpg }, {&C::oCMP, M::Zpg }, {&C::oDEC, M::Zpg }, {},
+		{&C::oINY         }, {&C::oCMP, M::Imm }, {&C::oDEX         }, {},
+		{&C::oCPY, M::Abs }, {&C::oCMP, M::Abs }, {&C::oDEC, M::Abs }, {},
+		{&C::oBNE, M::Rel }, {&C::oCMP, M::IndY}, {                 }, {},
+		{                 }, {&C::oCMP, M::ZpgX}, {&C::oDEC, M::ZpgX}, {},
+		{&C::oCLD         }, {&C::oCMP, M::AbsY}, {                 }, {},
+		{                 }, {&C::oCMP, M::AbsX}, {&C::oDEC, M::AbsX}, {},
+
+		{&C::oCPX, M::Imm }, {&C::oSBC, M::IndX}, {                 }, {},
+		{&C::oCPX, M::Zpg }, {&C::oSBC, M::Zpg }, {&C::oINC, M::Zpg }, {},
+		{&C::oINX         }, {&C::oSBC, M::Imm }, {&C::oNOP         }, {},
+		{&C::oCPX, M::Abs }, {&C::oSBC, M::Abs }, {&C::oINC, M::Abs }, {},
+		{&C::oBEQ, M::Rel }, {&C::oSBC, M::IndY}, {                 }, {},
+		{                 }, {&C::oSBC, M::ZpgX}, {&C::oINC, M::ZpgX}, {},
+		{&C::oSED         }, {&C::oSBC, M::AbsY}, {                 }, {},
+		{                 }, {&C::oSBC, M::AbsX}, {&C::oINC, M::AbsX}, {},
+	}};
 }
